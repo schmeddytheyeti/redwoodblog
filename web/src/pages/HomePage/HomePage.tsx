586f3796e85466232e46f786542c61ec7e400155
Sam Schmaedeka <samschmaedeka@Sams-MacBook-Pro.local>
2023-01-11 16:18:56 -0600

    second commit

---
+++ web/src/pages/HomePage/HomePage.tsx
@@ -0,0 +1,18 @@
+import { Link, routes } from '@redwoodjs/router'
+import { MetaTags } from '@redwoodjs/web'
+
+const HomePage = () => {
+  return (
+    <>
+      <MetaTags title="Sam's First Website" description="Home page" />
+
+      <h1>Sam's First Website</h1>
+      <p>
+        My default route is named <code>home</code>, link to me with `
+        <Link to={routes.home()}>Home</Link>`
+      </p>
+    </>
+  )
+}
+
+export default HomePage
